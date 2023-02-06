87bcabbf8137c0aff367a98c4d89f263a0afdeca
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-02-06 10:28:23 -0300

    Inclusão de texto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__Number');
+const pokemonImage = document.querySelector('.pokemon__image');
 
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -11,6 +13,8 @@ const renderPokemon = async (pokemon) => {
   const data = await fetchPokemon(pokemon);
 
   pokemonName.innerHTML = data.name;
+  pokemonNumber.innerHTML = data.id;
+  pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']
   
 }
 
