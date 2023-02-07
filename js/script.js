07ca36dd3a3710ff09bfe9700fe56d8f107eb521
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-02-06 23:22:29 -0300

    Inclusão de texto

--- js/script.js
+++ js/script.js
@@ -7,8 +7,10 @@ const input = document.querySelector('.input__search');
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 
+let searchPokemon = 1;
+
 const fetchPokemon = async (pokemon) => {
-  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
   if (APIResponse.status == 200) {
     const data = await APIResponse.json();
@@ -18,25 +20,36 @@ const fetchPokemon = async (pokemon) => {
 
 const renderPokemon = async (pokemon) => {
 
-  pokemonName.innerHTML = 'Loading...'
+  pokemonName.innerHTML = 'Loading...';
+  pokemonNumber.innerHTML = '';
 
   const data = await fetchPokemon(pokemon);
 
   if (data) {
-  pokemonName.innerHTML = data.name;
-  pokemonNumber.innerHTML = data.id;
-  pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_dafault'];
-  input.value = '';
-} else {
-  pokemonName.innerHTML = 'Not found :c';
-  pokemonNumber.innerHTML = '';
-}
-  
+   pokemonImage.style.display = 'none';
+   pokemonName.innerHTML = data.name;
+   pokemonNumber.innerHTML = data.id;
+   pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_dafault'];
+   input.value = '';
+  } else {
+   pokemonImage.style.display = 'none'
+   pokemonName.innerHTML = 'Not found :c';
+   pokemonNumber.innerHTML = '';
+  }
 }
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  renderPokemon(input.value);
+  renderPokemon(input.value.toLowerCase());
 });
 
-renderPokemon('1');
+buttonPrev.addEventListener('click', () => {
+  alert('prev clicked');
+});
+
+buttonNext.addEventListener('click', () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
